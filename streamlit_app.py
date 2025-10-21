677dcfe788d44c327e7586387dc5faf5b52b83bf
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2025-10-21 16:23:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2682,6 +2682,7 @@ else:
                 result_df = pd.concat([result_df, temp_df], ignore_index=True)
         
         # Display the final result_df
+        st.write(result_df.head())
         result_df = result_df.drop(columns=['batsman', 'debut_year', 'final_year','hundreds','fifties','thirties','highest_score','matches'])
         result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
         columns_to_convert = ['RUNS']
